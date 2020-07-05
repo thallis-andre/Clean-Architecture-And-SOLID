ba8bc28db31b284576fa0834713cdec2d519f4a4
thallis-andre <thallis-andre@hotmail.com>
2020-07-05 17:35:17 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,3 +17,21 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing params: name'))
   })
 })
+
+describe('Signup Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmatio: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing params: email'))
+  })
+})
