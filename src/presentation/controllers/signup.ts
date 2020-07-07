4a52cc1174b8ef46bfd5ed6cc5dddca89e2f679e
thallis-andre <thallis-andre@hotmail.com>
2020-07-07 20:55:06 -0300

    test: ensure SignupController return 400 if no password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -15,6 +15,10 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(`${field}`))
         }
       }
+
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
       const isValid = this.emaiValidator.isValid(httpRequest.body.email)
 
       if (!isValid) {
