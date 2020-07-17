6c8cfa630c28b260cd09a9cc8db67552cb9289c6
thallis-andre <thallis-andre@hotmail.com>
2020-07-17 15:24:47 -0300

    feat: ensure LogMongoRepository create an error log on success

--- src/data/protocols/log-error-repository.ts
+++ src/data/protocols/log-error-repository.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/method-signature-style */
 export interface LogErrorRepository {
-  log (stack: string): Promise<void>
+  logError (stack: string): Promise<void>
 }
