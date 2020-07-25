c704746c293f69e365357724ea3740c260024ec5
thallis-andre <thallis-andre@hotmail.com>
2020-07-24 21:19:58 -0300

    refactor: move httHelper to a folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -2,7 +2,7 @@ import { SignUpController } from './signup'
 import { ServerError, MissingParamError } from '../../errors'
 import { AddAccount, AddAccountModel, AccountModel } from './signup-protocols'
 import { HttpRequest } from '../../protocols'
-import { ok, serverError, badRequest } from './../../helpers/http-helper'
+import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 import { Validation } from '../../helpers/validators/validation'
 
 const makeAddAccount = (): AddAccount => {
