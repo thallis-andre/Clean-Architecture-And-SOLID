14bbc580f416441e84ba8de0f2563f0f2acde97d
thallis-andre <thallis-andre@hotmail.com>
2020-08-06 19:27:32 -0300

    test: ensure DbAuthentication return null if hashComparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -89,4 +89,11 @@ describe('DbAuthentication UseCase', () => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if hashComparer returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
