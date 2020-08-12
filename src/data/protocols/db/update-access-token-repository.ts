5fade4852a59de1c4995d4b976e5866dda23d569
thallis-andre <thallis-andre@hotmail.com>
2020-08-11 22:42:19 -0300

    refactor: rename update with updateAcessToken

--- src/data/protocols/db/update-access-token-repository.ts
+++ src/data/protocols/db/update-access-token-repository.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/method-signature-style */
 export interface UpdateAccessTokenRepository {
-  update (id: string, token: string): Promise<void>
+  updateAccessToken (id: string, token: string): Promise<void>
 }
